892977f01cd373c7be817cae8f1ee3a545e9e413
jpchaillot <jpchaillot@gmail.com>
2020-12-06 20:42:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ sudo apt-get install openjdk-8-jdk git -y
 sudo mkdir /home/minecraft
 cd /home/minecraft
 sudo wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-sudo java -Xms2048M -Xmsx3000M-jar BuildTools.jar --rev latest
-sudo java -Xms2048M -Xmsx3000M -jar /home/minecraft/spigot-x.x.jar nogui
+sudo java -Xms4000M -Xmsx6500M-jar BuildTools.jar --rev latest
+sudo java -Xms4000M -Xmsx6500M -jar /home/minecraft/spigot-x.x.jar nogui
 sudo sed -i 's/false/true/g' /home/minecraft/eula.txt
 cd /home/minecraft
-/usr/bin/java -Xms512M -Xmx1008M -jar ./spigot-x.x.jar nogui
+/usr/bin/java -Xms4000M -Xmsx6500M -jar ./spigot-x.x.jar nogui
 sudo chmod +x /home/minecraft/run-minecraft.sh
