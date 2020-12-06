1f6f59b51aa90a70be82941385524fd4e82c5f76
jpchaillot <jpchaillot@gmail.com>
2020-12-06 19:55:06 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install openjdk-8-jdk git -y
+sudo mkdir /home/minecraft
+cd /home/minecraft
+sudo wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+sudo sed -i 's/false/true/g' /home/minecraft/eula.txt
+sudo java -Xms512M -Xmx1008M -jar /home/minecraft/spigot-x.x.jar nogui
+sudo sed -i 's/false/true/g' /home/minecraft/eula.txt
+cd /home/minecraft
+/usr/bin/java -Xms512M -Xmx1008M -jar ./spigot-x.x.jar nogui
+sudo chmod +x /home/minecraft/run-minecraft.sh
